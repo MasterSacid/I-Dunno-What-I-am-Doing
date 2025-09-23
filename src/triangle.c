5ca963131c4d3ad442afd235cec93a0038082e25
sacidaktan <sacidaktan1@hotmail.com>
2025-09-23 14:25:49 +0300

    Z buffer added

--- src/triangle.c
+++ src/triangle.c
@@ -4,81 +4,133 @@
 
 
 
+    void drawTrianglePixel(
+    int x, int y, uint32_t color,
+    vec4_t pointA, vec4_t pointB, vec4_t pointC
+    ) {
+        vec2_t p = {x,y};
+        vec2_t a = vec4ToVec2(pointA);
+        vec2_t b = vec4ToVec2(pointB);
+        vec2_t c = vec4ToVec2(pointC);
 
 
-void fillFlatBottomTriangle(int x0,int y0,int x1,int y1,int x2,int y2, uint32_t color) {
-    //Find the inverse slopes of the legs
-    float inverseSlope1 = (float)(x1 - x0) / (y1 - y0);
-    float inverseSlope2 = (float)(x2 - x0) / (y2 - y0);
+        vec3_t weights = barycentricWeights(a,b,c,p);
 
-    //Start xStart and xEnd from the top vertex (Which is vertes (x0,y0))
-    float xStart = x0;
-    float xEnd = x0;
+        float alpha = weights.x;
+        float beta = weights.y;
+        float gamma = weights.z;
 
-    //Loop all the scanlines from top to bottom
-    for (int y = y0; y <= y2; y++) {
-        drawLine(xStart,y,xEnd,y,color);
-        xStart += inverseSlope1;
-        xEnd += inverseSlope2;
-    }
+        //Variables to store the interpolated values of U, V and 1/W for texel
+
+        float interpolatedReciprocalW;
+
+
+        //Interpolate the value of 1/w for the current texel
+        interpolatedReciprocalW =(1/pointA.w) * alpha + (1/pointB.w) * beta + (1/pointC.w) * gamma;
+
+
+        //Adjust 1/w so the pixels that are closer to the camera have smaller values.
+        interpolatedReciprocalW = 1.0 - interpolatedReciprocalW;
+
+
+        //Only draw the pixel if the depth value is the less than the one previously stored in the z buffer
+        if (interpolatedReciprocalW < zBuffer[(windowWidth * y) + x]) {
+            drawPixel(x,y,color);
+
+            //Update the z buffer value with 1/w of current pixel
+            zBuffer[(windowWidth * y ) + x] = interpolatedReciprocalW;
+        }
 }
 
-void fillFlatTopTriangle(int x0,int y0,int x1,int y1,int x2,int y2, uint32_t color) {
-    //Find the inverse slopes of the legs
-    float inverseSlope1 = (float)(x2 - x0) / (y2 - y0);
-    float inverseSlope2 = (float)(x2 - x1) / (y2 - y1);
-
-    //Start xStart and xEnd from the bottom vertex (Which is vertes (x2,y2))
-    float xStart = x2;
-    float xEnd = x2;
-
-    //Loop all the scanlines from bottom to top
-    for (int y = y2; y >= y0; y--) {
-        drawLine(xStart,y,xEnd,y,color);
-        xStart -= inverseSlope1;
-        xEnd -= inverseSlope2;
-    }
-
-}
 
 
-void drawFilledTriangle(int x0, int y0, int x1 , int y1, int x2, int y2,uint32_t color) {
+
+
+
+void drawFilledTriangle(int x0, int y0, float z0, float w0, int x1, int y1, float z1, float w1, int x2, int y2, float z2, float w2, uint32_t color) {
+
     //Sort it by ascending order (y0 < y1 <y2)
     if (y0 > y1) {
         intSwap(&y0,&y1);
         intSwap(&x0,&x1);
+        floatSwap(&z0,&z1);
+        floatSwap(&w0,&w1);
     }
     if (y1 > y2) {
         intSwap(&y1,&y2);
         intSwap(&x1,&x2);
+        floatSwap(&z1,&z2);
+        floatSwap(&w1,&w2);
     }
     if (y0 > y1) {
         intSwap(&y0,&y1);
         intSwap(&x0,&x1);
+        floatSwap(&z0,&z1);
+        floatSwap(&w0,&w1);
+    }
+    //Create vector points and texture coords after we sort the vertices
+    vec4_t pointA = {x0,y0,z0,w0};
+    vec4_t pointB = {x1,y1,z1,w1};
+    vec4_t pointC = {x2,y2,z2,w2};
+
+    //++++++++++==================++++++++++
+    //Render the upper part of the triangle
+    //++++++++++==================++++++++++
+    float invSlope1 = 0;
+    float invSlope2 = 0;
+
+    if (y1-y0 != 0)
+        invSlope1 = (float)(x1-x0) /(float) abs(y1-y0);
+
+    if(y2-y0!= 0)
+        invSlope2 = (float)(x2-x0) /(float) abs(y2-y0);
+
+    if (y1-y0 != 0) {
+        for (int y = y0; y<= y1; y++) {
+            int xStart = x1 + (y - y1) * invSlope1; //Since these are pixels it is better for them to be int
+            int xEnd = x0 + (y - y0) * invSlope2;
+
+            if (xEnd < xStart) {
+                intSwap(&xStart,&xEnd); // It should be always from left to right so we swap them
+            }
+
+            for (int x = xStart; x<xEnd;x++) {
+
+                drawTrianglePixel(x, y, color, pointA, pointB, pointC);
+
+            }
+        }
     }
 
-    if (y1==y2) {  //This two conditionals are added to aviod division by zero
-        //This will directly draw the bottom manually
-        fillFlatBottomTriangle(x0,y0,x1,y1,x2,y2,color);
+
+    //++++++++++==================++++++++++
+    //Render the bottom part of the triangle
+    //++++++++++==================++++++++++
+    invSlope1 = 0;
+    invSlope2 = 0;
+
+    if (y2-y1 != 0)
+        invSlope1 = (float)(x2-x1) /(float) abs(y2-y1);
+
+    if(y2-y0!= 0)
+        invSlope2 = (float)(x2-x0) /(float) abs(y2-y0);
+
+    if (y2-y1 != 0) {
+        for (int y = y1; y<= y2; y++) {
+            int xStart = x1 + (y - y1) * invSlope1; //Since these are pixels it is better for them to be int
+            int xEnd = x0 + (y - y0) * invSlope2;
+
+            if (xEnd < xStart) {
+                intSwap(&xStart,&xEnd); // It should be always from left to right so we swap them
+            }
+
+            for (int x = xStart; x<xEnd;x++) {
+
+                drawTrianglePixel(x, y, color, pointA, pointB, pointC);
+
+            }
+        }
     }
-    else if (y0 == y1) {
-        //Same excuse
-        fillFlatTopTriangle(x0,y0,x1,y1,x2,y2,color);
-    }
-
-    //No division by zero then calculate ...
-    else {
-        //Calculate the vertex Mx and My (In my notes)
-        int My = y1;
-        int Mx = ((float)((x2- x0) * (y1-y0)) / (float) (y2 - y0)) + x0;
-
-        //Draw flat - bottom triangle
-        fillFlatBottomTriangle(x0,y0,x1,y1,Mx,My,color);
-
-        //Draw flat -top triangle
-        fillFlatTopTriangle(x1,y1,Mx,My,x2,y2,color);
-    }
-
 
 }
 //======================TEXTURE STUFF========================
@@ -148,7 +200,18 @@ void drawTexel(
         int texX = abs((int)(interpolatedU * textureWidth)) % textureWidth; //Clamping
         int texY = abs((int)((1.0 - interpolatedV) * textureHeight)) % textureHeight;
 
-        drawPixel(x,y,texture[(textureWidth * texY) + texX]);
+        //Adjust 1/w so the pixels that are closer to the camera have smaller values.
+        interpolatedReciprocalW = 1.0 - interpolatedReciprocalW;
+
+
+        //Only draw the pixel if the depth value is the less than the one previously stored in the z buffer
+        if (interpolatedReciprocalW < zBuffer[(windowWidth * y) + x]) {
+            drawPixel(x,y,texture[(textureWidth * texY) + texX]);
+
+            //Update the z buffer value with 1/w of current pixel
+            zBuffer[(windowWidth * y ) + x] = interpolatedReciprocalW;
+        }
+
 
 
 }
